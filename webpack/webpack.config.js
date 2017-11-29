a25848704d08d453656445308eb4371928d000f0
Rodrigo Ferreira de Souza <rodfersou@gmail.com>
2017-11-29 10:53:15 -0200

    AJustes de funcionamento do tema (#151)
    * AJustes de funcionamento do tema
    
    * Add multiple theme structure
    
    * Review footer social icons sprite

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,85 +1,102 @@
 var CopyWebpackPlugin = require('copy-webpack-plugin');
 var SpritesmithPlugin = require('webpack-spritesmith');
-module.exports = {
-  entry: [
-    './app/padrao/brasilgovtemas.scss',
-    './app/padrao/preview.png',
-  ],
-  output: {
-    filename: 'brasilgovtemas.js',
-    path: __dirname + '/../src/brasil/gov/temas/themes'
-  },
-  plugins: [
-    new CopyWebpackPlugin([
-      { from: 'app/index.html', to: 'padrao/index.html' },
-      { from: 'app/rules.xml', to: 'padrao/rules.xml' },
-      { from: 'app/padrao/manifest.cfg', to: 'padrao/manifest.cfg' },
-    ], {
-    }),
-    new SpritesmithPlugin({
-      src: {
-        cwd: 'app/padrao/sprite',
-        glob: '*.png'
-      },
-      target: {
-        image: 'app/padrao/img/sprite.png',
-        css: 'app/padrao/_sprite.scss'
-      },
-      apiOptions: {
-        cssImageRef: './img/sprite.png'
-      }
-    })
-  ],
-  module: {
-    rules: [{
-      test: /\.scss$/,
-      use: [
-        {
-          loader: 'file-loader',
-          options: {
-            name: '[path][name].css',
-            context: 'app/'
-          }
+
+
+var createTheme = function(color) {
+  return {
+    entry: [
+      './app/brasilgovtemas.js',
+      './app/' + color + '/brasilgovtemas.scss',
+      './app/' + color + '/preview.png',
+    ],
+    output: {
+      filename: color + '/brasilgovtemas.js',
+      library: 'leitrabalhista',
+      libraryTarget: 'umd',
+      path: __dirname + '/../src/brasil/gov/temas/themes',
+      publicPath: '/++theme++' + color + '/'
+    },
+    plugins: [
+      new CopyWebpackPlugin([
+        { from: 'app/index.html', to: color + '/index.html' },
+        { from: 'app/rules.xml', to: color + '/rules.xml' },
+        { from: 'app/' + color + '/manifest.cfg', to: color + '/manifest.cfg' },
+      ], {
+      }),
+      new SpritesmithPlugin({
+        src: {
+          cwd: 'app/' + color + '/sprite',
+          glob: '*.png'
         },
-        'extract-loader',
-        'css-loader',
-        'postcss-loader',
-        'sass-loader'
-      ]
-    }, {
-      test: /.*\.(gif|png|jpe?g)$/i,
-      use: [
-        {
-          loader: 'file-loader',
-          options: {
-            name: '[path][name].[ext]',
-            context: 'app/',
-            publicPath: '../'
-          }
+        target: {
+          image: 'app/' + color + '/img/sprite.png',
+          css: 'app/' + color + '/_sprite.scss'
         },
-        {
-          loader: 'image-webpack-loader',
-          query: {
-            mozjpeg: {
-              progressive: true,
-            },
-            pngquant: {
-              quality: '65-90',
-              speed: 4
-            },
-            gifsicle: {
-              interlaced: false
-            },
-            optipng: {
-              optimizationLevel: 7
+        apiOptions: {
+          cssImageRef: './img/sprite.png'
+        }
+      })
+    ],
+    devtool: 'source-map',
+    module: {
+      rules: [{
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[path][name].css',
+              context: 'app/'
+            }
+          },
+          'extract-loader',
+          'css-loader',
+          'postcss-loader',
+          'sass-loader'
+        ]
+      }, {
+        test: /.*\.(gif|png|jpe?g)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[path][name].[ext]',
+              context: 'app/',
+              publicPath: function(url) {
+                return '/++theme++' + url;
+              }
+            }
+          },
+          {
+            loader: 'image-webpack-loader',
+            query: {
+              mozjpeg: {
+                progressive: true,
+              },
+              pngquant: {
+                quality: '65-90',
+                speed: 4
+              },
+              gifsicle: {
+                interlaced: false
+              },
+              optipng: {
+                optimizationLevel: 7
+              }
             }
           }
-        }
-      ]
-    }, {
-      test: /\.svg/,
-      exclude: /node_modules/,
-      use: 'svg-url-loader'
-    }]
-  }
-}
+        ]
+      }, {
+        test: /\.svg/,
+        exclude: /node_modules/,
+        use: 'svg-url-loader'
+      }]
+    }
+  };
+};
+
+module.exports = [
+  createTheme('padrao'),
+  createTheme('azul'),
+  createTheme('verde'),
+];
