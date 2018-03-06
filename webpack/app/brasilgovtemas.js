f32d6cef9b1d2d7ebdd406b626d95887f30706aa
STEFAN <hotpea@gmail.com>
2018-03-06 16:07:10 -0300

    Alterações de CSS e inclusão de textos

--- webpack/app/brasilgovtemas.js
+++ webpack/app/brasilgovtemas.js
@@ -12,11 +12,45 @@ $(document).ready(function(){
 
     // TODO: criar um arquivo separado para
     // Tour pelo site
-    $(".ico-navegacao").attr('data-intro', 'use o novo ícone de navegação em hamburguer para saciar sua fome de informações!').attr('data-step', 1);
-    $("#portal-searchbox").attr('data-intro', 'com a nova barra de busca vc pode buscar tudo o que precisa par a asua vida!').attr('data-step', 2);
-    $(".logo-governo").attr('data-intro', 'essa logo está com as cores zuadas, olhe bem e verás!').attr('data-step', 3);
-    $(".logo-acesso").attr('data-intro', 'essa tbem está, assim como a outra e ainda zoa a cor do footer!!!').attr('data-step', 4);
-    $(".documentFirstHeading").attr('data-intro', 'comece aqui a sua leitura super empolgante!').attr('data-step', 5);
+    $(".documentFirstHeading").attr(
+        'data-intro',
+        '<h3 style="color: blue; text-allign: center">O Portal Planalto Mudou</h3><br>' +
+        'A plataforma evoluiu para facilitar o acesso dos brasileiros a informações sobre a atuação' +
+        'do Presidente da República e do Executivo Federal. É o Governo do Brasil mais moderno e' +
+        'transparente.'
+    ).attr(
+        'data-hintPosition', 'bottom-middle'
+    );
+    $("#portal-searchbox").attr(
+        'data-intro',
+        '<h3 style="color: blue; text-allign: center">Busca Integrada</h3><br>' +
+        'Encontre informações e serviços de forma fácil e rápida'
+    ).attr(
+        'data-step', 2
+    );
+    $(".ico-navegacao").attr(
+        'data-intro',
+        '<h3 style="color: blue; text-allign: center">Nova Estrutura de Menu</h3><br>' +
+        'Clique para abrir o menu e encontrar os conteúdos do seu interesse'
+    ).attr(
+        'data-step', 3
+    );
+    $(".header-accessibility").attr(
+        'data-intro',
+        '<h3 style="color: blue; text-allign: center">Mais Ferramentas de Acessibilidade</h3><br>' +
+        'Contraste preto e branco, assim como tradutor de conteúdo para Língua Brasileira de Sinais(Libras),' +
+        ' estão disponíveis para pessoas com deficiência.'
+    ).attr(
+        'data-step', 4
+    );
+    $(".coluna-redes").attr(
+        'data-intro',
+        '<h3 style="color: blue; text-allign: center">Ficou mais fácil compartilhar</h3><br>' +
+        'Clique no botão da rede social que vc usa para publicar os conteúdos no seu perfil.'
+    ).attr(
+        'data-step', 5
+    );
+    // $(".documentFirstHeading").attr('data-intro', 'comece aqui a sua leitura super empolgante!').attr('data-step', 6);
 
     introJs().start();
 
