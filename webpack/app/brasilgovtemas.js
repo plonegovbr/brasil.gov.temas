62d41c1a3834613e4cc29592c7e299b2847cff29
Rodrigo Ferreira de Souza <rodfersou@gmail.com>
2018-03-06 15:14:57 -0300

    Review galeria de fotos view

--- webpack/app/brasilgovtemas.js
+++ webpack/app/brasilgovtemas.js
@@ -1,17 +1,21 @@
 import Contraste from './js/contrast.js';
+import GaleriaDeFotos from './js/albuns.js';
 
 
 $(document).ready(function(){
-    $(".ico-navegacao").click(function(){
-        $(".navigation-wrapper").toggleClass("ativo");
-    });
-    $(".ico-busca").click(function(){
-        $("#main-header").toggleClass("busca-ativa");
-        $("#portal-searchbox").toggleClass("ativo");
-    });
-    $('.tile-faq dt').on('click', function () {
-            $(this).next("dd").slideToggle();
-            $(this).toggleClass("aberto");
-        });
+  $(".ico-navegacao").click(function(){
+    $(".navigation-wrapper").toggleClass("ativo");
+  });
+  $(".ico-busca").click(function(){
+    $("#main-header").toggleClass("busca-ativa");
+    $("#portal-searchbox").toggleClass("ativo");
+  });
+  $('.tile-faq dt').on('click', function () {
+    $(this).next("dd").slideToggle();
+    $(this).toggleClass("aberto");
+  });
+  if ($('.template-galeria_de_fotos').length > 0) {
+    new GaleriaDeFotos();
+  }
 });
 
