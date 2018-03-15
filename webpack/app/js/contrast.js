e250b959fbd6d2ff4de64705383ae56783631de1
STEFAN <hotpea@gmail.com>
2018-03-15 10:34:33 -0300

    refatoração em código de variável do contraste(cookie)

--- webpack/app/js/contrast.js
+++ webpack/app/js/contrast.js
@@ -3,38 +3,15 @@ import 'jquery.cookie';
 // Adciona alto contras a página e inverte os ícones para sua versão de alto contraste
 jQuery(function($) {
   $('.link-contraste').click(
-    function(e) {
-      console.log($.cookie('contraste'));
-      if($.cookie('contraste') === null) {
+    function(e){
+      if($.cookie('contraste') != 'on') {
         $.cookie('contraste', 'on');
-
         $('body').addClass('contraste');
-
-        e.preventDefault();
-        return false;
-      }else {
-        if($.cookie('contraste') == 'on'){
-          $.cookie('contraste', 'off');
-
-          $('body').removeClass('contraste');
-
-          e.preventDefault();
-          return false;
-        }else{
-          $.cookie('contraste', 'on');
-
-          $('body').addClass('contraste');
-
-          e.preventDefault();
-          return false;
-        }
+      } else {
+        $.cookie('contraste', 'off');
+        $('body').removeClass('contraste');
       }
-  });
-
-  if($.cookie('contraste') == 'on') {
-
-    $('body').addClass('contraste').addClass('inverter');
-
-    return false;
-  }
+      e.preventDefault();
+      return false;
+    });
 });
