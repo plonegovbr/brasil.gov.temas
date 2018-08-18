bb21741349b5dc55c2eccf7d3c7cb0aff15b29c7
hvelarde <hector.velarde@gmail.com>
2018-08-17 21:42:30 -0300

    Refactor
    Rename components.

--- webpack/app/js/resultsfilter.js
+++ webpack/app/js/resultsfilter.js
@@ -1,19 +1,19 @@
-export default class FilterResults {
+export default class ResultsFilter {
   constructor() {
     $('#search-filter-toggle').on('click', this.toggleFilter.bind(this));
     // No search parameters -- start closed
     if (location.search === '') {
-      sessionStorage.filterResultsOpen = false;
+      sessionStorage.resultsFilterOpen = false;
     }
     // The filter box is open between searches
-    if (sessionStorage.filterResultsOpen === 'true') {
+    if (sessionStorage.resultsFilterOpen === 'true') {
       this.toggleFilter();
     }
   }
   toggleFilter(e) {
     if (typeof e !== 'undefined') {
       e.preventDefault();
-      sessionStorage.filterResultsOpen = (sessionStorage.filterResultsOpen === 'false');
+      sessionStorage.resultsFilterOpen = (sessionStorage.resultsFilterOpen === 'false');
     }
     $('#search-filter-toggle').toggleClass('closed');
     $('#search-results-bar dd.actionMenuContent').toggleClass('closed');
