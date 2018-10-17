86eef2d489eb19e9ae27276fb51d5b029c52dcb3
Rodrigo Ferreira de Souza <rodfersou@gmail.com>
2018-10-17 12:27:44 -0300

    Fix social like start position

--- webpack/app/js/sociallike.js
+++ webpack/app/js/sociallike.js
@@ -2,26 +2,24 @@ export default class SocialLike {
   constructor() {
     this.$el = $('#viewlet-social-like');
     this.$container = $('#main-content');
-    this.bindEvents();
     this.scrolling = false;
-    this.elementStartPosition = this.bottomOffset(this.$el);
-    this.onScroll();
-  }
-  bindEvents() {
-    this.$el.stop(true, true);
-    $(document).on('scroll', this.onScroll.bind(this));
+    this.elementStartPosition = this.$container.offset().top + this.$el.height();
+    this.bindEvents();
   }
   set isScrolling(scrolling) {
-    if (this.scrolling != scrolling) {
+    if (this.scrolling !== scrolling) {
       this.$el.toggleClass('is-scrolling', scrolling);
       this.scrolling = scrolling;
     }
   }
+  bindEvents() {
+    this.$el.stop(true, true);
+    $(document).on('scroll', this.onScroll.bind(this));
+    this.onScroll();
+  }
   onScroll() {
     let bottomElement = this.elementStartPosition + $(document).scrollTop();
-    this.isScrolling = (bottomElement > this.bottomOffset(this.$container));
-  }
-  bottomOffset($el) {
-    return $el.offset().top + $el.height();
+    let bottomContainer = this.$container.offset().top + this.$container.height();
+    this.isScrolling = (bottomElement > bottomContainer);
   }
 }
